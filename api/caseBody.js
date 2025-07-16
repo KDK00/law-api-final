5b91f305cea3c0cf8d889d9925b5856f847e106f
KDK00 <mentor0119@naver.com>
2025-07-17 02:25:21 +0900

    Initial commit of final project

---
+++ api/caseBody.js
@@ -0,0 +1,12 @@
+import axios from 'axios';
+export default async function handler(req, res) {
+  const { id } = req.query;
+  const url = `http://www.law.go.kr/DRF/lawService.do?OC=mentor0119&target=prec&type=XML&ID=${id}`;
+  try {
+    const response = await axios.get(url, { responseType: 'text' });
+    res.setHeader('Content-Type', 'application/xml; charset=utf-8');
+    res.status(200).send(response.data);
+  } catch (error) {
+    res.status(500).json({ error: "API 호출 실패" });
+  }
+}
